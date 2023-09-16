406409227fa5fa5d2ae128dd17ba56e7dbd21db7
Dhakchayini <a.n.swathyraj@gmail.com>
2023-09-16 16:02:47 +0530

    first commit

--- src/test/java/facebook/facebook.java
+++ src/test/java/facebook/facebook.java
@@ -1,17 +1,33 @@
 package facebook;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class facebook {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 	
 		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://in.bookmyshow.com/explore/home/chennai");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.makemytrip.com/");
 		driver.manage().window().maximize();
+		
+		
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("a.n.swathyraj@gmail.com");
+		driver.findElement(By.xpath("//span[text()='Continue']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a[text()='Reset Password']")).click();
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		Thread.sleep(20000);
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//input[@type='password'])[2]")).sendKeys("Mahamitu@14");
+		driver.findElement(By.xpath("//span[text()='Reset Password']")).click();
+		driver.getWindowHandle();
+		
+		
 	}
 
 }
